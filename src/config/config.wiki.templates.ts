4b96fc69d968954176feb8629c17e28629c29289
Chris Daniells <chrisdaniells@gmail.com>
2019-10-19 00:17:38 +0100

    stuff

--- src/config/config.wiki.templates.ts
+++ src/config/config.wiki.templates.ts
@@ -46,7 +46,36 @@ const biography: IPage = {
     last_updated: 0,
 }
 
+const fauna: IPage = {
+    id: '',
+    url: '',
+    title: '',
+    category: Categories.Ecology,
+    subcategory: SubCategories.Fauna,
+    images: {
+        main: '',
+        other: [],
+    },
+    details: [
+        {
+            label: 'Animal Kingdom',
+            value: '',
+            link: false,
+        },
+        {
+            label: 'Animal Order',
+            value: '',
+            link: false,
+        },
+    ],
+    body: '<h1>Etymology</h1><p></p><h1>Taxonomy and phylogeny</h1><p></p><h1>Characteristics</h1><p></p><h2>Size</h2><p></p><h2>Head</h2><p></p><h2>Body</h2><p></p><h2>Limbs</h2><p></p><h2>Feet</h2><p></p><h2>Other</h2><p></p><h1>Behaviour</h1><h2>Habitat</h2><p></p><h2>Social Organisation</h2><p></p><h2>Diet</h2><p></p><h2>Assication With Humans</h2><p></p>',
+    preface: '',
+    date_created: 0,
+    last_updated: 0,
+}
+
 export default {
     'base': base ,
     'biography': biography,
+    'fauna': fauna,
 };
